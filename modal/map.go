81f94d1a14c7737733530b43219c066bdbe41a91
TangMin88 <65007800+TangMin88@users.noreply.github.com>
2020-05-25 18:14:42 +0800

    Add files via upload

---
+++ modal/map.go
@@ -0,0 +1,3 @@
+package modal
+
+var Maps map[string]interface{}
--- modal/page.go
+++ modal/page.go
@@ -2,15 +2,17 @@ package modal
 
 //Home 发送到主页
 type Page struct {
-	Judge   	bool 		//判断用户是否登录
+	Judge 		bool 	
 	Session 	*Session
 	BooK 		[]*Book 	//每页查询出的图书存放的切片
 	PageNo 		int64 		//当前页
 	PageSize 	int64 		//每页显示的条数
 	TotalPage 	int64		//总页数，通过计算得到
 	TotalRecord int64	    //总记录数，通过查询数据库得到
-	JudgeShop	bool      //判断是否是店主
+	SJudge 		bool
 	Shop		*Shop
+	Pricemin	float64
+	Pricemax	float64
 } 
 
 //判断是否有上一页
